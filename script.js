2fde2ce733a219fcf023063d616205c453edd0f4
faqihfnf <faqih.fnf@gmail.com>
2024-09-23 10:46:54 +0700

    until step 97

--- script.js
+++ script.js
@@ -90,15 +90,24 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeaponIndex++;
-    goldText.innerText = gold;
-  }
-  else{
-    text.innerText = "You now have a new weapon."
+  // Check if the player has not reached the last weapon
+  if (currentWeaponIndex < weapons.length) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the best weapon and cannot buy another one.";
   }
 }
+
 
 function fightSlime() {
 }
